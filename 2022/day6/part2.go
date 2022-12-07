a06e34d2bff496937d49b5f0e11f7a30f2855c9b
Sean Swehla <sean@sweh.la>
2022-12-06 19:49:25 -0500

    2022 day 6

---
+++ 2022/day6/part2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+
+	var buf []byte
+
+	var idx int
+	for len(buf) < 14 || hasDup(buf) {
+		idx++
+		b, err := r.ReadByte()
+		if err == io.EOF || b == '\n' {
+			log.Fatalf("no seq found by index %d", idx)
+		}
+		if err != nil {
+			log.Fatalf("error reading byte: %s", err.Error())
+		}
+
+		if len(buf) < 14 {
+			buf = append(buf, b)
+			continue
+		}
+
+		buf = append(buf[1:], b)
+	}
+
+	fmt.Println(idx)
+}
+
+func atoIdx(b byte) int { return int(b) - 97 }
+func hasDup(buf []byte) bool {
+	for i, b1 := range buf[:len(buf)-1] {
+		for _, b2 := range buf[i+1:] {
+			if b1 == b2 {
+				return true
+			}
+		}
+	}
+	return false
+}
