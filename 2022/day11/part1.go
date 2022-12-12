0d7c5e8851a1247147f5f7203b44c239ba5c4c09
Sean Swehla <sean@sweh.la>
2022-12-12 01:56:13 -0500

    2022 Day 11 part 1

---
+++ 2022/day11/part1.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/Knetic/govaluate"
+)
+
+type Monkey struct {
+	ID           int
+	Items        []int
+	testFn       func(int) bool
+	opFn         func(int) int
+	TrueTgt      int
+	FalseTgt     int
+	itemsHandled int
+}
+
+func main() {
+	var monkeys []*Monkey
+	buf := bufio.NewReader(os.Stdin)
+
+	for {
+		if _, err := buf.Peek(1); err == io.EOF {
+			break
+		}
+		monkeys = append(monkeys, parse(buf))
+	}
+
+	for i := 0; i < 20; i++ {
+		for _, monkey := range monkeys {
+			for _, item := range monkey.Items {
+				monkey.itemsHandled++
+				item = monkey.opFn(item) / 3
+				tgt := monkey.FalseTgt
+				if monkey.testFn(item) {
+					tgt = monkey.TrueTgt
+				}
+				monkeys[tgt].Items = append(monkeys[tgt].Items, item)
+			}
+			monkey.Items = monkey.Items[:0]
+		}
+	}
+
+	var max, pen int
+	for i, m := range monkeys {
+		fmt.Printf("Monkey %d: handled %d items\n", i, m.itemsHandled)
+		if m.itemsHandled > max {
+			pen, max = max, m.itemsHandled
+			continue
+		}
+		if m.itemsHandled > pen {
+			pen = m.itemsHandled
+		}
+	}
+
+	fmt.Printf("%d %d %d", max, pen, max*pen)
+}
+
+func parse(r *bufio.Reader) *Monkey {
+	var m Monkey
+	if n, err := fmt.Fscanf(r, "Monkey %d:\n", &m.ID); err != nil || n != 1 {
+		log.Fatalf("parsed %d items for ID; got error %q", n, err.Error())
+	}
+
+	itemsStr, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatalf("error reading starting items: %q", err.Error())
+	}
+
+	m.Items = parseItems(strings.TrimRight(itemsStr, "\n"))
+
+	opStr, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatalf("error reading operation: %q", err.Error())
+	}
+
+	m.opFn = parseOpFn(strings.TrimRight(opStr, "\n"))
+
+	testStr, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatalf("error reading test: %q", err.Error())
+	}
+
+	m.testFn = parseTestFn(strings.TrimRight(testStr, "\n"))
+
+	if n, err := fmt.Fscanf(r, "    If true: throw to monkey %d\n", &m.TrueTgt); err != nil || n != 1 {
+		log.Fatalf("parsed %d items for true target; got error %q", n, err.Error())
+	}
+
+	if n, err := fmt.Fscanf(r, "    If false: throw to monkey %d\n", &m.FalseTgt); err != nil || n != 1 {
+		log.Fatalf("parsed %d items for false target; got error %q", n, err.Error())
+	}
+
+	blank, err := r.ReadString('\n')
+	if err != nil && err != io.EOF {
+		log.Fatalf("error skipping blank line: %q", err.Error())
+	}
+
+	if strings.TrimRight(blank, "\n") != "" {
+		log.Fatalf("expected blank line, found %q", blank)
+	}
+
+	return &m
+}
+
+func parseItems(str string) []int {
+	if !strings.HasPrefix(str, "  Starting items: ") {
+		log.Fatalf("expected items line, found %q", str)
+	}
+
+	itemStrs := strings.Split(str[18:], ", ")
+	var items []int
+	for _, itemStr := range itemStrs {
+		n, err := strconv.Atoi(itemStr)
+		if err != nil {
+			log.Fatalf("error parsing item %q: %q", itemStr, err.Error())
+		}
+		items = append(items, n)
+	}
+	return items
+}
+
+func parseOpFn(str string) func(int) int {
+	if !strings.HasPrefix(str, "  Operation: new = ") {
+		log.Fatalf("expected operation line, found %q", str)
+	}
+
+	expr, err := govaluate.NewEvaluableExpression(str[19:])
+	if err != nil {
+		log.Fatalf("error parsing op: %q", err.Error())
+	}
+
+	return func(i int) int {
+		val, err := expr.Evaluate(map[string]interface{}{"old": i})
+		if err != nil {
+			log.Fatalf("error evaluating function: %q", err.Error())
+		}
+
+		valf64, ok := val.(float64)
+		if !ok {
+			log.Fatalf("error casting %T as float64", val)
+		}
+
+		return int(valf64)
+	}
+}
+
+func parseTestFn(str string) func(int) bool {
+	var d int
+	if n, err := fmt.Sscanf(str, "  Test: divisible by %d", &d); err != nil || n != 1 {
+		log.Fatalf("parsed %d items; got error %q", n, err.Error())
+	}
+	return func(i int) bool { return i%d == 0 }
+}
